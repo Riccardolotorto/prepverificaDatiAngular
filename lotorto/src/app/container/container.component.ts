bd1a1fc54fdc213707dbd5bf9e91f62a54832a28
Riccardolotorto <lotorto.riccardo@iisgalvanimi.edu.it>
2023-12-18 11:47:25 +0000

    punto 2

--- lotorto/src/app/container/container.component.ts
+++ lotorto/src/app/container/container.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-container',
   templateUrl: './container.component.html',
   styleUrls: ['./container.component.css']
 })
-export class ContainerComponent {
+export class ContainerComponent implements OnInit{
+      cars: String[];
+      constructor() {
+        this.cars = ['fiat', 'ford', 'audi', 'mercedes', 'tesla', 'mazda', 'renault', 'citroen', 'ferrari', 'lamborghini'];
+      }
+      ngOnInit () {
 
-}
+      }
+} 
